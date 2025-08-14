3bddee40590536a052700082f756f829f1d7a480
Scott Aubrey <scott@aubrey.org.uk>
2025-08-14 15:17:27 +0100

    wire up the s3 stream and turn on content-disposition again

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-const config = {
-  s3: {
-    bucket: "prod-elife-published"
-  }
-}
+import { s3 } from "bun";
 
 Bun.serve({
   // `routes` requires Bun v1.2.3+
   routes: {
     // Dynamic routes
-    "/download/:id/:filename": req => {
+    "/download/:id/:filename": async req => {
       const cdnUri = URL.parse(atob(req.params.id));
 
       if (!cdnUri) {
@@ -21,11 +17,16 @@ Bun.serve({
 
       const canonicalUri = cdnUri.searchParams.get('canonicalUri');
 
-      const s3Path = `s3://${config.s3.bucket}${cdnUri.pathname}`;
+      const s3file = s3.file(cdnUri.pathname);
+      if (!(await s3file.exists())) {
+        return new Response("Not Found", { status: 404 });
+      }
 
-      const response = new Response(`Decode ${req.params.id} to CDN uri ${cdnUri} and get content from ${s3Path} and return as ${req.params.filename} with canonicalUri of ${canonicalUri}`);
+      const stream = s3file.stream();
+
+      const response = new Response(stream);
       response.headers.set('Link', `<${canonicalUri}>; rel="canonical"`);
-      // response.headers.set('Content-Disposition', `attachment; filename="${req.params.filename}"`)
+      response.headers.set('Content-Disposition', `attachment; filename="${req.params.filename}"`)
       return response;
     },
   },
