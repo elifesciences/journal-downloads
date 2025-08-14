642f37b765b714c6bacf25e6192829c183d1027b
Scott Aubrey <scott@aubrey.org.uk>
2025-08-14 15:40:09 +0100

    implement hash checking

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 import { s3 } from "bun";
+import { createHmac } from "crypto";
+
+const uriSignerSecret = process.env.SECRET
 
 Bun.serve({
   // `routes` requires Bun v1.2.3+
   routes: {
     // Dynamic routes
     "/download/:id/:filename": async req => {
+      const url = URL.parse(req.url);
+      if (!url) {
+        return new Response("Not Found", { status: 404 });
+      }
+
+      const hash = url?.searchParams.get('_hash');
+
+      if (hash) {
+        if (!uriSignerSecret) {
+          return new Response("Cannot verify request", { status: 500 });
+        }
+
+        //override parts of the URL that we know will be different
+        url.host = "elifesciences.org"
+        url.protocol = "https"
+        url.port = "443"
+        url.search = ""
+
+        const newHash = createHmac("sha256", uriSignerSecret)
+                      .update(url.toString())
+                      .digest("base64");
+
+        if (newHash !== hash) {
+          return new Response("Not Acceptable", { status: 406 });
+        }
+      }
+
       const cdnUri = URL.parse(atob(req.params.id));
 
       if (!cdnUri) {
