5f561ccfbbf2b7a9a6bbc2bcc957093c4c192ed1
Scott Aubrey <scott@aubrey.org.uk>
2025-08-15 07:53:55 +0100

    mock file exists, and test behaviour when the file exists and when it doesn't

--- src/routes.test.ts
+++ src/routes.test.ts
@@ -2,11 +2,13 @@ import { describe, it, expect, mock } from "bun:test"
 import { BunRequest, S3Client } from "bun";
 import { createRoutes } from "./routes";
 
+const fileExistsMock = mock(() => true)
+
 // Create a mock S3 client that simulates the behavior we need for testing.
 const mockS3: S3Client = {
   // @ts-expect-error we only need to mock the methods we use
   file: (pathname: string) => ({
-    exists: async () => true,
+    exists: fileExistsMock,
     stream: () => new Blob(["file content"]).stream(),
   }),
 };
@@ -34,5 +36,42 @@ describe('routes', async () => {
     const res = await routes["/download/:id/:filename"](req);
     expect(res.status).toBe(406);
     expect(await res.text()).toBe("Not Acceptable");
+  });
+
+  it("should return 404 file when a file does not exist in s3", async () => {
+    const fileUrl = `https://cdn.elifesciences.org/test.jpg`;
+    const validID = btoa(fileUrl);
+    const filename = "test.jpg";
+    fileExistsMock.mockReturnValue(false);
+
+    const req = new Request(`https://example.com/downloads/${validID}/${filename}`) as BunRequest;
+    req.params = {
+      id: validID,
+      filename,
+    };
+    const res = await routes["/download/:id/:filename"](req);
+
+    expect(res.status).toBe(404);
+    expect(await res.text()).toBe("Not Found");
+
+    //reset mock
+    fileExistsMock.mockReturnValue(true);
+  });
+
+  it("should return a file when it exists in s3", async () => {
+    const fileUrl = `https://cdn.elifesciences.org/test.jpg`;
+    const validID = btoa(fileUrl);
+    const filename = "test.jpg";
+
+    const req = new Request(`https://example.com/downloads/${validID}/${filename}`) as BunRequest;
+    req.params = {
+      id: validID,
+      filename,
+    };
+    const res = await routes["/download/:id/:filename"](req);
+
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("file content");
+    expect(res.headers.get("Content-Disposition")).toBe(`attachment; filename="${filename}"`);
   });
 });
